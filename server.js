b03d4d6f96a0590359ed46659d63ad78504f7612
Gaurav Tiwari <30768224+gtiwari1993@users.noreply.github.com>
2017-08-08 08:36:02 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ var articleOne = {
 };
 
 function createTemplate(data){
+var title = data.title;
+var date = data.date;
+var heading = data.heading;
+var content = data.content;
+    
+    
 var htmlTemplate = `<html>
     <head>
     <title>
