b8b692f59ae28942e731367021eb5e3329b913a7
Gaurav Tiwari <30768224+gtiwari1993@users.noreply.github.com>
2017-08-08 21:47:34 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -100,6 +100,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
+    //articles[articleName] == {} content object for article one
   res.send(createTemplate(articles[articleName]));
 });
 
