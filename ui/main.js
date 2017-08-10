242ffacc5dc092f8f84e576f605cdd5539add4a4
Gaurav Tiwari <30768224+gtiwari1993@users.noreply.github.com>
2017-08-10 01:59:10 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,23 +16,38 @@ function moveRight()
 
 
 
-img.onclick = function(){
+img.onclick = function()
+{
     var interval  = setInterval(moveRight,50);
     
 }
 
 //counter code
 var button = document.getElementById('counter');
-var counter = 0;
 
-button.onclick = function() {
-    //make a request to counter endpoint
+button.onclick = function() 
+{
+//create a request object
+    var request - new XMLHttpRequest();
     
     //capture the response and store it in a variable
-    
-    //Reder the variable in correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    request.onreadystatechange =function()
+    {
+        if(request.readyState === XMLHttpRequest.DONE)
+        {
+            //take some action
+            if(request.status ===200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+     
+            }
+        }
+    };
+    //make the request
+    request.open('GET','http://http://tiwarigaurav320.imad.hasura-app.io/counter',true);
+    request.send(null);
+
     
 };
