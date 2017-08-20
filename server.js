1d4f9299c30e4c38cd1d23e27578a404f5da8a97
Gaurav Tiwari <30768224+gtiwari1993@users.noreply.github.com>
2017-08-20 06:41:42 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,13 +115,13 @@ app.get('/counter', function (req, res){
 });
 
 
-app.get('/:articleName', function (req, res) {
+/*app.get('/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
-    var articleName = req.param.articleName;
+    var articleName = req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
-
+*/
 var pool = new Pool(config);
 app.get('/test-db', function (req, res)
 {
