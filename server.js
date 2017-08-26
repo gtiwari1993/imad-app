e46f56a3035f33255733426e9d3ae0f83f3038a2
Gaurav Tiwari <30768224+gtiwari1993@users.noreply.github.com>
2017-08-26 11:10:57 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ res.send(hashedString);
 
 
 app.post('/create-user',function(req,res) {
-    var username = req.bondy.username;
+    var username = req.body.username;
     var password = req.body.password;
     var salt = crypto.getrandomBytes(128).toString('hex');
     var dbString = hash('password',salt);
